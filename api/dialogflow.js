8f1db572ad3b14db43beae29e2d20714cdedf616
BookBotEsg <bookbotesg@gmail.com>
2025-04-09 12:44:32 -0300

    Update dialogflow.js

--- api/dialogflow.js
+++ api/dialogflow.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const agentMessage = req.body.queryResult.queryText;
 
   const response = {
-    fulfillmentText: Oi! Você disse: "${agentMessage}". Isso veio do webhook Vercel!,
+    fulfillmentText: `Oi! Você disse: "${agentMessage}". Isso veio do webhook Vercel!`,
   };
 
   res.status(200).json(response);
